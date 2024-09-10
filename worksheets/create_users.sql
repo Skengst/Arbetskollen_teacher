001c218f362f17e6703aca816da8c972de6a3080
Dimitris1886 <dimaklimenko90@gmail.com>
2024-09-10 13:24:41 +0200

    Setup snowflake

---
+++ worksheets/create_users.sql
@@ -0,0 +1,27 @@
+USE ROLE USERADMIN; -- is responsible for managing user/accounts/roles
+
+CREATE USER IF NOT EXISTS Daniel --  this user will be responsible for the extraction and loading processes,
+PASSWORD = 'Password1#'
+LOGIN_NAME = 'Daniel'
+MUST_CHANGE_PASSWORD = true
+DEFAULT_WAREHOUSE = job_ads_wh;
+
+CREATE USER Samuel
+PASSWORD = 'Password2#' -- changed to Passworddbtrole123##
+LOGIN_NAME = 'Samuel'
+MUST_CHANGE_PASSWORD = true
+DEFAULT_WAREHOUSE = job_ads_wh;
+
+CREATE USER Jacob
+PASSWORD = 'Password3#'
+LOGIN_NAME = 'Jacob'MUST_CHANGE_PASSWORD = true
+DEFAULT_WAREHOUSE = job_ads_wh;
+
+SHOW USERS;
+
+drop user dbt_user;
+
+CREATE USER Dimitris_2
+PASSWORD = 'Password4#'
+LOGIN_NAME = 'Dimitris_2'MUST_CHANGE_PASSWORD = true
+DEFAULT_WAREHOUSE = job_ads_wh;
