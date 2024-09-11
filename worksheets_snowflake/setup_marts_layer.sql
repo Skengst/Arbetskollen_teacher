d754af53696a83f75dd523230d187a47889411c9
Jacob Bodin <Jacob.bodin@5mail.com>
2024-09-11 21:36:28 +0200

    set up marts layer

---
+++ worksheets_snowflake/setup_marts_layer.sql
@@ -0,0 +1,32 @@
+USE ROLE SYSADMIN;
+
+USE DATABASE JOB_ADS_DB;
+
+CREATE SCHEMA IF NOT EXISTS marts;
+
+USE ROLE SECURITYADMIN;
+
+GRANT USAGE,
+CREATE TABLE,
+CREATE VIEW ON SCHEMA JOB_ADS_DB.marts TO ROLE teacher_dbt_role;
+
+-- grant CRUD and select tables and views
+GRANT SELECT,
+INSERT,
+UPDATE,
+DELETE ON ALL TABLES IN SCHEMA JOB_ADS_DB.marts TO ROLE teacher_dbt_role;
+GRANT SELECT ON ALL VIEWS IN SCHEMA JOB_ADS_DB.marts TO ROLE teacher_dbt_role;
+
+-- grant CRUD and select on future tables and views
+GRANT SELECT,
+INSERT,
+UPDATE,
+DELETE ON FUTURE TABLES IN SCHEMA JOB_ADS_DB.marts TO ROLE teacher_dbt_role;
+GRANT SELECT ON FUTURE VIEWS IN SCHEMA JOB_ADS_DB.marts TO ROLE teacher_dbt_role;
+
+-- manual test
+USE ROLE teacher_dbt_role;
+USE SCHEMA JOB_ADS_DB.marts;
+CREATE TABLE test (id INTEGER);
+SHOW TABLES;
+DROP TABLE TEST;
