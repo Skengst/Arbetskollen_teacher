fa17f9586ab16b4e348d5f866a0071652e4a263b
Skengst <skengst@gmail.com>
2024-09-13 15:09:12 +0200

    Corrected spelling of "auxiliary"

--- dbt/arbetskollen_data_pipeline/models/dim/dim_auxilliary.sql
+++
@@ -1,9 +0,0 @@
-WITH src_auxilliary AS (SELECT * FROM {{ref('src_auxilliary')}})
-
-SELECT 
-    {{ dbt_utils.generate_surrogate_key(['id','experience_required'])}} AS auxilliary_attributes_id,
-    experience_required,
-    driving_license_required,
-    car_required,
-
-FROM src_auxilliary
