1fc9ba418be2bbf0dd6a439bdde8688083741a10
Dimitris1886 <dimaklimenko90@gmail.com>
2024-09-14 21:06:12 +0200

    Adjusted test_dashboard and run_dashboard

--- streamlit_dashboard/test_dashboard.py
+++ streamlit_dashboard/test_dashboard.py
@@ -4,7 +4,8 @@ import altair as alt
 from connect_data_warehouse import query_job_listings
 
 def layout():
-    st.title('Job Listings Dashboard')
+    st.set_page_config(layout="wide")
+    st.subheader('Job Listings Dashboard')
 
     # Hämta data
     df = query_job_listings()
@@ -26,25 +27,30 @@ def layout():
     col3.metric("Genomsnittlig relevans", round(avg_relevance, 2))
     col4.metric("Vanligaste yrket", most_common_occupation)
 
-    # Bar chart över antalet jobb per arbetsgivare
-    st.write("### Antal jobb per arbetsgivare")
-    job_per_employer = df.groupby('EMPLOYER_NAME').size().reset_index(name='ANTAL_JOBB')
-    bar_chart = alt.Chart(job_per_employer).mark_bar().encode(
-        x='EMPLOYER_NAME:N',  # Nominal data
-        y='ANTAL_JOBB:Q'  # Quantitative data
-    ).properties(width=600)
-    st.altair_chart(bar_chart)
+    chart_1, chart_2 = st.columns(2)
+    with chart_1:
 
-    # Line chart över relevans över tid
-    st.write("### Relevans över tid")
-    df['APPLICATION_DEADLINE'] = pd.to_datetime(df['APPLICATION_DEADLINE'], errors='coerce')
-    relevance_over_time = df.groupby('APPLICATION_DEADLINE')['RELEVANCE'].mean().reset_index()
+        # Bar chart över antalet jobb per arbetsgivare
+        st.write("### Antal jobb per arbetsgivare")
+        job_per_employer = df.groupby('EMPLOYER_NAME').size().reset_index(name='ANTAL_JOBB')
+        bar_chart = alt.Chart(job_per_employer).mark_bar().encode(
+            x='EMPLOYER_NAME:N',  # Nominal data
+            y='ANTAL_JOBB:Q'  # Quantitative data
+        ).properties(width=600)
+        st.altair_chart(bar_chart)
 
-    line_chart = alt.Chart(relevance_over_time).mark_line().encode(
-        x='APPLICATION_DEADLINE:T',  # Time data
-        y='RELEVANCE:Q'  # Quantitative data
-    ).properties(width=600)
-    st.altair_chart(line_chart)
+    with chart_2:
+
+        # Line chart över relevans över tid
+        st.write("### Relevans över tid")
+        df['APPLICATION_DEADLINE'] = pd.to_datetime(df['APPLICATION_DEADLINE'], errors='coerce')
+        relevance_over_time = df.groupby('APPLICATION_DEADLINE')['RELEVANCE'].mean().reset_index()
+
+        line_chart = alt.Chart(relevance_over_time).mark_line().encode(
+            x='APPLICATION_DEADLINE:T',  # Time data
+            y='RELEVANCE:Q'  # Quantitative data
+        ).properties(width=600)
+        st.altair_chart(line_chart)
 
     # Text om de mest frekventa yrkena
     st.write("### Mest förekommande yrken")
