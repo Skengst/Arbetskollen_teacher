85cbff41dc13ae3eba370ad55e29f544c7829014
Dimitris1886 <dimaklimenko90@gmail.com>
2024-09-14 13:19:08 +0200

    Adjusted dashboard layout

--- dbt/streamlit_dashboard/dashboard.py
+++ dbt/streamlit_dashboard/dashboard.py
@@ -2,9 +2,15 @@ import streamlit as st
 from connect_data_warehouse import query_job_listings
 
 def layout():
+    st.set_page_config(
+    page_title="NAME OF THE COMPANY",
+    page_icon="🧊",
+    layout="wide")
     st.title('Job Listings Dashboard')
     df = query_job_listings()
+    st.text("Placeholder of number of ads ")
     st.write(df)
     
 if __name__ == "__main__":
     layout()
+    
