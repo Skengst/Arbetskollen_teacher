f9a0b14ebd29bce6dbcf1682a73e6f5cf3406040
Daniel Sjoholm <danielblomqvistsjoholm@gmail.com>
2024-09-15 22:20:58 +0200

    Added some small things

--- worksheets_snowflake/dlt/check_loaded_data.sql
+++ worksheets_snowflake/dlt/check_loaded_data.sql
@@ -7,3 +7,16 @@ USE DATABASE job_ads_db;
 USE SCHEMA staging;
 
 SELECT COUNT(*) AS AmountOfRows FROM jobsearch_teacher_data;
+
+USE ROLE TEACHER_DBT_ROLE;
+
+USE WAREHOUSE job_ads_wh;
+
+USE DATABASE job_ads_db;
+
+USE SCHEMA WAREHOUSE;
+
+SELECT employer_id, COUNT(*)
+FROM dim_employer
+GROUP BY employer_id
+HAVING COUNT(*) > 1;
